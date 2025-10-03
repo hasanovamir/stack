cc6e2b1dcc3aa4cbb221c56c4b595d1d0ccd9949
hasanovamir <ali01h72@gmail.com>
2025-10-04 02:11:07 +0300

    visual update

--- stack.cpp
+++ stack.cpp
@@ -1,74 +1,69 @@
 #include "stack.h"
 
-stack_err_t stack_init (stack_t* stk, int user_size)
+stack_err_t stack_init (stack_t* stk, int init_size)
 {
     DEBUG_ASSERT (stk != NULL);
-    if (user_size < 1)
-        return invalid_value;
+
+    if (init_size < 1)
+    {
+        return STACK_INVALID_VALUE;
+    }
     
-    int array_size = 0;
-    int array_min_size = 64;
-    
-    if (user_size > array_min_size)
-        array_size = user_size + 2;
-    else
-        array_size = array_min_size;
+    int array_size = (init_size > array_min_size) ? init_size + 2: array_min_size;
 
     stk->data = (stack_val_t*)calloc (array_size, sizeof (stack_val_t));
-    if (stk->data == NULL)
-        return allocete_err;
 
-    RETURN_IF_ERR (stack_fill (stk, array_size)); //memset
+    if (stk->data == NULL)
+    {
+        return STACK_ALLOCATION_ERR;
+    }
+
+    stk->capacity = array_size - 2;
+    stk->size = 0;
+    
+    RETURN_IF_ERR (stack_fill (stk)); //memset
 
     STACK_OK (stk);
 
-    return success;
+    return STACK_SUCCESS;
 }
 
+//--------------------------------------------------------------------------------
 
 stack_err_t stack_push (stack_t* stk, stack_val_t value)
 {
     STACK_OK (stk);
 
-    if (stk->size == stk->capasity - 1)
-    {
-        stk->data = (stack_val_t*)realloc (stk->data, stk->capasity * 2);
-        stk->capasity = 2 * stk->capasity - 2;
-        if (stk->data == NULL) 
-            return allocete_err;
-    }
+    RETURN_IF_ERR (upsize_array_if_need (stk));
 
     stk->data[++stk->size] = value;
 
     STACK_OK (stk);
 
-    return success;
+    return STACK_SUCCESS;
 }
 
+//--------------------------------------------------------------------------------
 
 stack_err_t stack_pop (stack_t* stk, stack_val_t* val_pointer)
 {
     STACK_OK (stk);
     if (stk->size == 0)
-        return no_el_to_pop;
-
-    int array_min_size = 64;
-    if (stk->size <= stk->capasity / 4 && stk->capasity > array_min_size * 2)
     {
-        stk->data = (stack_val_t*)realloc (stk->data, stk->capasity / 2);
-        stk->capasity = stk->capasity / 2 - 2;
-        if (stk->data == NULL) 
-            return allocete_err;
+        return STACK_NO_EL_TO_POP;
     }
+
+    RETURN_IF_ERR (downsize_array_if_need (stk));
 
     *val_pointer = stk->data[stk->size];
     stk->data[stk->size--] = POIZON;
 
     STACK_OK (stk);
     
-    return success;
+    return STACK_SUCCESS;
 }
 
+//--------------------------------------------------------------------------------
 
 stack_err_t stack_destroy (stack_t* stk)
 {
@@ -76,109 +71,155 @@ stack_err_t stack_destroy (stack_t* stk)
 
     free (stk->data);
 
-    return success;
+    return STACK_SUCCESS;
 }
 
+//--------------------------------------------------------------------------------
 
 const char* error_code_to_string (stack_err_t status)
 {
     switch (status)
     {
-        case data_ruined:
-            return "data ruined";
-            break;
-        case allocete_err:
-            return "allocete error";
-            break;
-        case invalid_value:
-            return "invalid value";
-            break;
-        case scan_num_err:
-            return "scan num err";
-            break;
-        case scan_oper_err:
-            return "scan operation err";
-            break;
-        case no_el_to_pop:
-            return "no elements to pop";
-            break;
-        case success:
-            return "correct function operation";
-            break;
-        case finish_scan:
-            return "end scan";
-            break;
+        ERRCASE (STACK_DATA_RUINED   );
+        ERRCASE (STACK_ALLOCATION_ERR);
+        ERRCASE (STACK_NO_EL_TO_POP  );
+        ERRCASE (STACK_SUCCESS       );
+        ERRCASE (STACK_INVALID_VALUE );
         default:
-            return "correct function operation";
-            break;
+            return "SUCCESS";
     }
 }
 
+//-----------------------------------------------------------------------------
 
 void stack_dump (stack_t* stk)
 {
     printf ("Stack [%p]\n{\n", stk);
 
-    int tmp = 0;
-    if (stk->capasity < 20)
-        tmp = stk->capasity;
-    else   
-        tmp = 20;
+    int tmp = (stk->capacity < 100) ? stk->capacity : 100;
 
-    printf ("\tsize = %d\n", stk->size);
-    printf ("\tcapasity = %d\n", stk->capasity);
-    printf ("\tdata[%p]\n\t{\n", stk->data);
+    printf ("\tsize = %d\n",     stk->size     );
+    printf ("\tcapacity = %d\n", stk->capacity );
+    printf ("\tdata[%p]\n\t{\n", stk->data     );
 
     if (stk->data[0] == KANAREIKA)
+    {
         printf ("\t\t[0] = KANAREIKA\n");
-    else    
+    }
+    else
+    {
         printf ("\t\t[0] = %d (not KANAREIKA)\n", stk->data[0]);
+    }
 
     for (int i = 1; i < tmp + 1; i++)
     {
         if (stk->data[i] == POIZON)
+        {
             printf ("\t\t[%d] = POIZON\n", i);
-        else    
+        }
+        else
+        {
             printf ("\t\t*[%d] = %d\n", i, stk->data[i]);
+        }
     }
 
-    if (stk->data[stk->capasity + 1] == KANAREIKA)
-        printf ("\t\t[%d] = KANAREIKA\n", stk->capasity - 1);
-    else    
-        printf ("\t\t[%d] = %d (not KANAREIKA)\n", stk->capasity + 1, stk->data[stk->capasity + 1]);
+    if (stk->data[stk->capacity + 1] == KANAREIKA)
+    {
+        printf ("\t\t[%d] = KANAREIKA\n", stk->capacity + 1);
+    }
+    else
+    {
+        printf ("\t\t[%d] = %d (not KANAREIKA)\n", stk->capacity + 1, stk->data[stk->capacity + 1]);
+    }
 
     printf ("\t}\n}\n");
 }
 
+//--------------------------------------------------------------------------------
 
 stack_err_t stack_verify (stack_t* stk)
 {
-    DEBUG_ASSERT (stk != 0);
-    DEBUG_ASSERT (stk->data != 0);
+    DEBUG_ASSERT (stk != NULL      );
+    DEBUG_ASSERT (stk->data != NULL);
 
-    if (stk->data[stk->capasity + 1] != KANAREIKA || stk->data[0] != KANAREIKA)
-        return data_ruined;
+    if (stk->data[stk->capacity + 1] != KANAREIKA || stk->data[0] != KANAREIKA)
+    {
+        return STACK_DATA_RUINED;
+    }
 
-    return success;
+    return STACK_SUCCESS;
 }
 
+//--------------------------------------------------------------------------------
 
-stack_err_t stack_fill (stack_t* stk, int array_size)
+stack_err_t stack_fill (stack_t* stk)
 {
     DEBUG_ASSERT (stk != 0);
     DEBUG_ASSERT (stk->data != 0);
 
-    stk->capasity = array_size - 2;
-
-    stk->size = 0;
-
     stk->data[0] = KANAREIKA;
-    stk->data[array_size - 1] = KANAREIKA;
+    stk->data[stk->capacity + 1] = KANAREIKA;
 
-    for (int i = 1; i < array_size - 1; i++)
+    for (int i = 1; i < stk->capacity + 1; i++)
+    {
         stk->data[i] = POIZON;
+    }
 
     STACK_OK (stk);
 
-    return success;
+    return STACK_SUCCESS;
 }
+
+//--------------------------------------------------------------------------------
+
+stack_err_t upsize_array_if_need (stack_t* stk)
+{
+    DEBUG_ASSERT (stk != NULL      );
+    DEBUG_ASSERT (stk->data != NULL);
+
+    if (stk->size == stk->capacity - 1)
+    {
+        stack_val_t* old_data = stk->data;
+
+        stk->data = (stack_val_t*)realloc (stk->data, stk->capacity * 2);
+
+        if (stk->data == NULL)
+        {
+            stk->data = old_data;
+
+            return STACK_ALLOCATION_ERR;
+        }
+
+        stk->capacity = 2 * stk->capacity - 2;
+    }
+
+    return STACK_SUCCESS;
+}
+
+//--------------------------------------------------------------------------------
+
+stack_err_t downsize_array_if_need (stack_t* stk)
+{
+    DEBUG_ASSERT (stk != NULL      );
+    DEBUG_ASSERT (stk->data != NULL);
+
+    if (stk->size <= stk->capacity / 4 && stk->capacity > array_min_size * 2)
+    {
+        stack_val_t* old_data = stk->data;
+
+        stk->data = (stack_val_t*)realloc (stk->data, stk->capacity / 2);
+
+        if (stk->data == NULL)
+        {
+            stk->data = old_data;
+
+            return STACK_SUCCESS;
+        }
+
+        stk->capacity = stk->capacity / 2 - 2;
+    }
+
+    return STACK_SUCCESS;
+}
+
+//--------------------------------------------------------------------------------
--- stack.h
+++ stack.h
@@ -1,103 +1,102 @@
 #ifndef STACK_H
 #define STACK_H
 
+//--------------------------------------------------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <sys/stat.h>
 
-#define POIZON 0x11223344
+//--------------------------------------------------------------------------------
+
+#define POIZON    0x11223344
 #define KANAREIKA 0x55667788
+
+//--------------------------------------------------------------------------------
+
+const int array_min_size = 64;
 
 typedef int stack_val_t;
 
-typedef enum
-{
-    PUSH = 1,
-    OUT = 2,
-    ADD = 3,
-    SUB = 4,
-    DIV = 5,
-    MUL = 6,
-    HLT = 7,
-    sqvRT = 8
-} operation_t;
+//--------------------------------------------------------------------------------
 
 typedef enum
 {
-    finish_scan = 8,
-    no_el_to_pop = 7,
-    scan_num_err = 6,
-    scan_oper_err = 5,
-    data_ruined = 4,
-    allocete_err = 3,
-    invalid_value = 2,
-    success = 0
+    STACK_SUCCESS        = 0,
+    STACK_NO_EL_TO_POP   = 2,
+    STACK_DATA_RUINED    = 3,
+    STACK_ALLOCATION_ERR = 4,
+    STACK_INVALID_VALUE  = 5,
 } stack_err_t;
 
-typedef struct
+//--------------------------------------------------------------------------------
+
+struct stack_t
 {
     stack_val_t* data;
     int size;
-    int capasity;
-} stack_t;
+    int capacity; // spellchecker
+};
 
-typedef struct
-{
-    stack_val_t value;
-    operation_t operation;
-} operation_params;
+//--------------------------------------------------------------------------------
 
-stack_err_t stack_init (stack_t* stk, int user_size);
-stack_err_t stack_push (stack_t* stk, stack_val_t value);
-stack_err_t stack_pop (stack_t* stk, stack_val_t* val_pointer);
-stack_err_t stack_destroy (stack_t* stk);
-const char* error_code_to_string (stack_err_t status);
-void stack_dump (stack_t* stk);
-stack_err_t stack_verify (stack_t* stk);
-stack_err_t scan_input_operation (operation_params* parametrs);
-stack_err_t processing_scaned_oper (operation_params* parametrs, stack_t* stk);
-stack_err_t scan_and_pocessing (stack_t* stk);
-stack_err_t stack_fill (stack_t* stk, int array_size);
+stack_err_t stack_init             (stack_t* stk, int init_size           );
+stack_err_t stack_push             (stack_t* stk, stack_val_t value       );
+stack_err_t stack_pop              (stack_t* stk, stack_val_t* val_pointer);
+stack_err_t stack_destroy          (stack_t* stk                          );
+const char* error_code_to_string   (stack_err_t status                    );
+void        stack_dump             (stack_t* stk                          );
+stack_err_t stack_verify           (stack_t* stk                          );
+stack_err_t stack_fill             (stack_t* stk                          );
+stack_err_t upsize_array_if_need   (stack_t* stk                          );
+stack_err_t downsize_array_if_need (stack_t* stk                          );
 
-
+//--------------------------------------------------------------------------------
 
 #ifndef N_DEBUG
 #define DEBUG_ASSERT(cond)\
 if (!cond)\
 {\
-    fprintf(stderr, "%s in %s: %d func:%s\n", #cond, __FILE__, __LINE__, __PRETTY_FUNCTION__);\
+    fprintf(stderr, "%s in %s:%d func:%s\n", #cond, __FILE__, __LINE__, __PRETTY_FUNCTION__);\
     exit(1);\
 }
 #else
 #define DEBUG_ASSERT() 
-#endif
+#endif //N_DEBUG
 
-
+//--------------------------------------------------------------------------------
 
 #define RETURN_IF_ERR(status) do \
 {\
     stack_err_t _status_val_ = status;\
     if (_status_val_)\
     {\
-        fprintf(stderr, "%s in %s: %d func:%s\n", error_code_to_string (_status_val_), __FILE__, __LINE__, __PRETTY_FUNCTION__);\
+        fprintf(stderr, "%s in %s:%d func:%s\n", error_code_to_string (_status_val_), __FILE__, __LINE__, __PRETTY_FUNCTION__);\
         return _status_val_;\
     }\
 } while (0)
 
-
+//--------------------------------------------------------------------------------
 
 #define STACK_OK(stack) do\
 {\
     stack_err_t _status_ = stack_verify (stack);\
     if (_status_)\
     {\
-        printf ("StackDump colled from %s: %d\n", __FILE__, __LINE__);\
+        printf ("StackDump called from %s:%d\n", __FILE__, __LINE__);\
         stack_dump (stack);\
         return _status_;\
     }\
 }while (0);
 
+//--------------------------------------------------------------------------------
 
+#define ERRCASE(enum)\
+    case enum:\
+        return #enum;\
 
-#endif
+//--------------------------------------------------------------------------------
+
+#endif // STACK_H
